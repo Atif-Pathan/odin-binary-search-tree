cceafddffcc3e76389109301450acc5dfb8e958e
Atif Pathan <atifkhan.p1@gmail.com>
2025-01-30 15:26:58 -0700

    create main Node and Tree class with skeleton functions and mergesort for sorting array before BST creation

---
+++ main.js
@@ -0,0 +1,61 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+
+    mergeSort(array) {
+        // base case
+        if(array.length <= 1) {
+            return array;
+        } else {
+            // split in two halves and sort each half
+            let mid = Math.floor(array.length / 2);
+            let sortedLeft = this.mergeSort(array.slice(0, mid));
+            let sortedRight = this.mergeSort(array.slice(mid));
+            
+            //merge sortef halves
+            let merged = [];
+            
+            // keep merging until one of the arrays becomes empty, causing loop to end
+            while (sortedLeft.length && sortedRight.length) {
+                merged.push(sortedLeft[0] < sortedRight[0] ? sortedLeft.shift() : sortedRight.shift());
+            }
+            
+            // since one of the left or right arrays will be empty, doing this will append the non empty remaining array
+            return merged.concat(sortedLeft, sortedRight);;
+        }
+    }
+
+    insert(value) {
+
+    }
+
+    delete(value) {
+
+    }
+     
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.rightChild !== null) {
+      prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.leftChild !== null) {
+      prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+ 
+const t = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(t.root);
